90bcb7ca7818904c2a4c6f93496427076d2d1a57
HOFFMAN <19hoffmani@gmail.com>
2018-09-05 08:37:30 -0500

    add stuff

---
+++ app.js
@@ -0,0 +1,13 @@
+function sayHello ()
+{
+    console.log("Something silly and bye!");
+}
+sayHello();
+
+function sayHola()
+{
+    let hi = "Hola silly"
+    return hi;
+}
+let message = sayHola()
+console.log(message);
