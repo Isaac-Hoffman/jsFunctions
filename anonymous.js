d905026e04d960349351593ed0a5d830c4504d0f
HOFFMAN <19hoffmani@gmail.com>
2018-09-05 09:17:01 -0500

    final notes

--- anonymous.js
+++ anonymous.js
@@ -3,3 +3,17 @@ const hello = function(name){//anonymous function
     return message;
 }
 console.log(hello("Dory"));
+
+//fat arrow function =>
+const aloha = (name) =>{
+    console.log(name);
+}
+aloha("Nemo");
+
+const howzit = name => `Howzit, ${name}!`;
+console.log(howzit("Nani"));
+
+console.log(Math.max(4.5, 5));
+console.log(Math.pow(6, 3));
+console.log(Math.sqrt(36));
+console.log(Math.ceil(Math.random() * 10));
