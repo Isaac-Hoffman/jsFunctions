36430958bd2e2dc3eaafb1825e3c7ab704d0fbca
HOFFMAN <19hoffmani@gmail.com>
2018-09-05 08:55:08 -0500

    anonymous function

---
+++ anonymous.js
@@ -0,0 +1,5 @@
+const hello = function(name){//anonymous function
+    const message = `Hello, ${name}!`;
+    return message;
+}
+console.log(hello("Dory"));
--- app.js
+++ app.js
@@ -11,3 +11,20 @@ function sayHola()
 }
 let message = sayHola()
 console.log(message);
+
+function howzit()
+{
+    let hi = "howzit"
+    return hi;
+    console.log("I will not print")
+}
+console.log(howzit());
+
+function sayAloha(name)
+{
+    const message = `Aloha, ${name}!`;
+    return message;
+}
+
+console.log(sayAloha("Lilo"));
+console.log(sayAloha("Stitch"));
